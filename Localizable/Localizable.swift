494f81a9faeaf3ff8ef822bd39596c65a6aafe73
Roman Sorochak <roman.sorochak@gmail.com>
2017-06-23 23:48:10 +0300

    - implemented Localizable extensions to simplify working with localized resources: strings/images - implemented ability to change language in the app

---
+++ Localizable/Localizable.swift
@@ -0,0 +1,114 @@
+//
+//  String+Localizable.swift
+//  Localizable
+//
+//  Created by Roman Sorochak on 6/23/17.
+//  Copyright © 2017 MagicLab. All rights reserved.
+//
+
+import UIKit
+
+
+private let appleLanguagesKey = "AppleLanguages"
+
+
+enum Language: String {
+    
+    case english = "en"
+    case arabic = "ar"
+    case ukrainian = "uk"
+    
+    var semantic: UISemanticContentAttribute {
+        switch self {
+        case .english, .ukrainian:
+            return .forceLeftToRight
+        case .arabic:
+            return .forceRightToLeft
+        }
+    }
+    
+    
+    static var language: Language {
+        get {
+            if let languageCode = UserDefaults.standard.string(forKey: appleLanguagesKey),
+                let language = Language(rawValue: languageCode) {
+                return language
+            } else {
+                let preferredLanguage = NSLocale.preferredLanguages[0] as String
+                let index = preferredLanguage.index(
+                    preferredLanguage.startIndex,
+                    offsetBy: 2
+                )
+                guard let localization = Language(
+                    rawValue: preferredLanguage.substring(to: index)
+                    ) else {
+                        return Language.english
+                }
+                
+                return localization
+            }
+        }
+        set {
+            guard language != newValue else {
+                return
+            }
+            
+            //change language in the app
+            //the language will be changed after restart
+            UserDefaults.standard.set([newValue.rawValue], forKey: appleLanguagesKey)
+            UserDefaults.standard.synchronize()
+            
+            //Changes semantic to all views
+            //this hack needs in case of languages with different semantics: leftToRight(en/uk) & rightToLeft(ar)
+            UIView.appearance().semanticContentAttribute = newValue.semantic
+            
+            //initialize the app from scratch
+            //show initial view controller
+            //so it seems like the is restarted
+            //NOTE: do not localize storboards
+            //After the app restart all labels/images will be set
+            //see extension String below
+            UIApplication.shared.windows[0].rootViewController = UIStoryboard(
+                name: "Main",
+                bundle: nil
+                ).instantiateInitialViewController()
+        }
+    }
+}
+
+
+extension String {
+    
+    var localized: String {
+        return Bundle.localizedBundle.localizedString(forKey: self, value: nil, table: nil)
+    }
+    
+    var image: UIImage? {
+        return localizedImage()
+            ?? localizedImage(type: ".png")
+            ?? localizedImage(type: ".jpg")
+            ?? localizedImage(type: ".jpeg")
+            ?? UIImage(named: self)
+    }
+    
+    private func localizedImage(type: String = "") -> UIImage? {
+        guard let imagePath = Bundle.localizedBundle.path(forResource: self, ofType: type) else {
+            return nil
+        }
+        return UIImage(contentsOfFile: imagePath)
+    }
+}
+
+extension Bundle {
+    //Here magic happens
+    //when you localize resources: for instance Localizable.strings, images
+    //it creates different bundles
+    //we take appropriate bundle according to language
+    static var localizedBundle: Bundle {
+        let languageCode = Language.language.rawValue
+        guard let path = Bundle.main.path(forResource: languageCode, ofType: "lproj") else {
+            return Bundle.main
+        }
+        return Bundle(path: path)!
+    }
+}
---
+++ Localizable/MainVC.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Localizable
+//
+//  Created by Roman Sorochak on 6/23/17.
+//  Copyright © 2017 MagicLab. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        label.text = "main_page_language".localized
+        imageView.image = "flag".image
+        button.setTitle(
+            "main_page_change_language".localized,
+            for: .normal
+        )
+    }
+    
+    @IBAction func changeLanguage(_ sender: Any) {
+        let alert = UIAlertController(
+            title: "alert_change_language_title".localized,
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        
+        func addActionLanguage(language: Language) {
+            alert.addAction(
+                UIAlertAction(
+                    title: language.rawValue.localized,
+                    style: UIAlertActionStyle.default,
+                    handler: { _ in
+                        Language.language = language
+                })
+            )
+        }
+        addActionLanguage(language: Language.english)
+        addActionLanguage(language: Language.ukrainian)
+        addActionLanguage(language: Language.arabic)
+        
+        alert.addAction(
+            UIAlertAction(
+                title: "alert_cancel".localized,
+                style: UIAlertActionStyle.cancel,
+                handler: nil
+            )
+        )
+        present(alert, animated: true, completion: nil)
+    }
+}
+
